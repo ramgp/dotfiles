55d013e4101b0b6968c59234a45c9163d5c27e90
Ramon Garcia-Perez <ramgarcia@outlook.com>
2021-03-20 18:36:24 -0400

    Make bat man pager if available

--- roles/zsh/env.zsh
+++ roles/zsh/env.zsh
@@ -1,3 +1,8 @@
+if (($+commands[bat])); then
+  export MANROFFOPT="-c"
+  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+fi
+
 export TERM=xterm-256color
 export EDITOR='nvim'
 export VISUAL='nvim'
