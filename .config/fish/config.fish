c13f68f700865ee30395fb21d410c78d8319ca6d
Ramon Garcia-Perez <ramgarcia@outlook.com>
2023-01-08 14:58:12 -0500

    feat: add fish navigation key bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -40,6 +40,13 @@ if status --is-interactive
   set theme $XDG_CONFIG_HOME/fish/themes/kanagawa.fish
   test -r $theme; and source $theme
 
+  bind \$ end-of-line
+  bind ^ beginning-of-line
+  bind -M insert \ce end-of-line
+  bind -M insert \ca beginning-of-line
+  bind -M insert \e\[A history-prefix-search-backward
+  bind -M insert \e\[B history-prefix-search-forward
+
   zoxide init fish | source
 end
 
