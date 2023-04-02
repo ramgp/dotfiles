b5f830a0b28e4a615dfb7e1657ce68b8b9ecde08
Ramon Garcia-Perez <ramgarcia@outlook.com>
2023-04-02 16:55:35 -0400

    chore: improve fish shell config

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -4,8 +4,8 @@ alias -s pg='ps -ef | rg'
 alias -s pkill="pkill -9 -f "
 alias -s lj='jobs'
 
-alias -s tmux="TERM=xterm-256color command tmux -f $XDG_CONFIG_HOME/tmux/.tmux.conf"
-alias -s tmx="TERM=xterm-256color command tmux -f $HOME/.dotfiles/.tmux.conf"
+alias -s tmux="TERM=xterm-256color command tmux"
+alias -s tmx="TERM=xterm-256color command tmux"
 alias -s myip='curl ifconfig.co'
 
 alias -s 0ad="$HOME/prg/0ad/binaries/system/pyrogenesis"
@@ -13,35 +13,46 @@ alias -s 0ad="$HOME/prg/0ad/binaries/system/pyrogenesis"
 # git
 alias -s g=git
 alias -s gl="git pull --rebase"
-alias -s gf="git fetch origin"
+alias -s gf="git fetch origin --prune"
 alias -s lgcm="git rev-parse --short HEAD"
 alias -s cgb="git rev-parse --abbrev-ref HEAD"
 alias -s gsud="git switch develop && git pull -r"
 alias -s gud="git update-ref refs/heads/develop origin/develop"
 alias -s gsb="git switch"
+alias -s gbc="git switch -c"
+alias -s gm="git merge"
+alias -s ga="git add"
 
-if type -t exa
-  alias -s ll="exa --icons -l --group-directories-first"
-  alias -s llt="exa --tree --icons -l --group-directories-first"
-  alias -s la="exa --icons -la --group-directories-first"
-  alias -s lat="exa --tree --icons -la --group-directories-first"
-  alias -s l="exa --icons -1a --group-directories-first"
-  alias -s lo="la -snew"
-  alias -s tree="exa --tree"
+if command -sq exa
+    alias -s ll="exa --icons -l --group-directories-first"
+    alias -s llt="exa --tree --icons -l --group-directories-first"
+    alias -s la="exa --icons -la --group-directories-first"
+    alias -s lat="exa --tree --icons -la --group-directories-first"
+    alias -s l="exa --icons -1a --group-directories-first"
+    alias -s lo="la -snew"
+    alias -s tree="exa --tree"
 end
 
-if type -t docker-compose
-  alias -s dk=docker-compose
-  alias -s dku="docker-compose up -d"
-  alias -s dkd="docker-compose down"
+if command -sq docker-compose
+    alias -s dk=docker-compose
+    alias -s dku="docker-compose up -d"
+    alias -s dkd="docker-compose down"
 end
 
-if type -t podman
-  alias -s docker=podman
-  alias -s pod=podman
-  alias -s pm="podman machine"
-  alias -s pmd="podman machine stop"
-  alias -s pmu="podman machine start"
+if command -sq podman
+    alias -s docker=podman
+    alias -s pod=podman
+    alias -s pm="podman machine"
+    alias -s pmd="podman machine stop"
+    alias -s pmu="podman machine start"
+end
+
+if command -sq tldr
+    alias -s tl='tldr --list | fzf --preview "tldr {1} --color=always" --preview-window=right,68% | xargs tldr'
+end
+
+if command -sq cowsay
+    alias -s moo='fortune | cowsay'
 end
 
 alias -s serve='python3 -m http.server'
@@ -51,4 +62,3 @@ alias -s jj='pbpaste | jsonpp | pbcopy'
 alias -s hxstat="history 0 | awk '{print $2}' | sort | uniq -c | sort -n -r | head"
 
 alias -s se='podman run --rm -it -p 4444:4444 -p 7900:7900 -e SE_NODE_MAX_SESSIONS=4 -e SCREEN_WIDTH=1920 -e SCREEN_HEIGHT=1080 --shm-size 3g seleniarm/standalone-chromium:latest'
-
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,44 +9,44 @@ set -g theme_date_format "+%H:%M %a %m/%d"
 set -gx EDITOR lvim
 set -gx VISUAL lvim
 
+# set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
 if status --is-interactive
-  fish_vi_key_bindings
+    fish_vi_key_bindings
 
-  eval (/opt/homebrew/bin/brew shellenv)
+    eval (/opt/homebrew/bin/brew shellenv)
 
-  fish_add_path -Pmp $HOME/.cargo/bin /opt/homebrew/opt/curl/bin
-  fish_add_path -Pa $HOME/.local/bin /usr/local/bin 
+    fish_add_path -Pmp $HOME/.cargo/bin /opt/homebrew/opt/curl/bin
+    fish_add_path -Pa $HOME/.local/bin /usr/local/bin
 
-  set -Ux PYENV_ROOT $HOME/.pyenv
-  set -gx N_PREFIX $HOME/.local/n
-  set -gx PNPM_HOME $XDG_DATA_HOME/pnpm
+    set -Ux PYENV_ROOT $HOME/.pyenv
+    set -gx N_PREFIX $HOME/.local/n
+    set -gx PNPM_HOME $XDG_DATA_HOME/pnpm
 
-  fish_add_path -Pa $PYENV_ROOT/bin $HOME/.dotfiles/bin $N_PREFIX/bin $XDG_DATA_HOME/pnpm
+    fish_add_path -Pa $PYENV_ROOT/bin $HOME/.dotfiles/bin $N_PREFIX/bin $XDG_DATA_HOME/pnpm
 
-  set aliases $XDG_CONFIG_HOME/fish/aliases.fish
-  test -r $aliases && source $aliases 1> /dev/null
+    if not test -r $XDG_CONFIG_HOME/fish/functions/hxstat.fish
+        set aliases $XDG_CONFIG_HOME/fish/aliases.fish
+        test -r $aliases && source $aliases 1>/dev/null
 
-  set r1_aliases $HOME/.r1_aliases.fish
-  test -r $r1_aliases && source $r1_aliases 1> /dev/null
+        set r1_aliases $HOME/.r1_aliases.fish
+        test -r $r1_aliases && source $r1_aliases 1>/dev/null
+    end
 
-  function multicd
-    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
-  end
+    function multicd
+        echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+    end
 
-  abbr --add dotdot --regex '^\.\.+$' --function multicd
+    abbr --add dotdot --regex '^\.\.+$' --function multicd
 
-  set -g theme_color_scheme dark 
+    set -g theme_color_scheme dark
 
-  set theme $XDG_CONFIG_HOME/fish/themes/kanagawa.fish
-  test -r $theme; and source $theme
+    bind -M default \$ end-of-line
+    bind -M default ^ beginning-of-line
+    bind -M insert \ce end-of-line
+    bind -M insert \ca beginning-of-line
+    bind -M insert \e\[A history-prefix-search-backward
+    bind -M insert \e\[B history-prefix-search-forward
 
-  bind -M default \$ end-of-line
-  bind -M default ^ beginning-of-line
-  bind -M insert \ce end-of-line
-  bind -M insert \ca beginning-of-line
-  bind -M insert \e\[A history-prefix-search-backward
-  bind -M insert \e\[B history-prefix-search-forward
-
-  zoxide init fish | source
+    zoxide init fish | source
 end
-
