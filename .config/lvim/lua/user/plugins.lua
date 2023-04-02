2f1b27d9f38b9c16e1c6b3c307baa8c8fc36118a
Ramon Garcia-Perez <ramgarcia@outlook.com>
2023-04-02 16:53:49 -0400

    chore: update lvim tab formatting

--- .config/lvim/lua/user/plugins.lua
+++ .config/lvim/lua/user/plugins.lua
@@ -1,66 +1,65 @@
 -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
-    { "rebelot/kanagawa.nvim" },
-    { "LunarVim/colorschemes" },
-    -- {
-    --   "EdenEast/nightfox.nvim",
-    --   config = function()
-    --     require("nightfox").setup({
-    --       options = {
-    --         styles = {
-    --           comments = "italic",
-    --           keywords = "italic",
-    --           types = "italic,bold",
-    --         }
-    --       }
-    --     })
-    --   end
-    -- },
-    {
-        "kylechui/nvim-surround",
-        event = "VeryLazy",
-        version = "*", -- Use for stability; omit to use `main` branch for the latest features
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end
-    },
-    {
-        "FotiadisM/tabset.nvim",
-        event = "VeryLazy",
-        config = function()
-            require("tabset").setup(
-                {
-                    defaults = {
-                        tabwidth = 2,
-                        expandtab = true
-                    },
-                    languages = {
-                        lua = {
-                            tabwidth = 4,
-                            expandtab = true
-                        },
-                        go = {
-                            tabwidth = 4,
-                            expandtab = false
-                        },
-                        {
-                            filetypes = {
-                                "javascript",
-                                "typescript",
-                                "javascriptreact",
-                                "typescriptreact",
-                                "json",
-                                "yaml"
-                            },
-                            config = {
-                                tabwidth = 2
-                            }
-                        }
-                    }
-                }
-            )
-        end
-    },
+  { "rebelot/kanagawa.nvim" },
+  -- {
+  --   "EdenEast/nightfox.nvim",
+  --   config = function()
+  --     require("nightfox").setup({
+  --       options = {
+  --         styles = {
+  --           comments = "italic",
+  --           keywords = "italic",
+  --           types = "italic,bold",
+  --         }
+  --       }
+  --     })
+  --   end
+  -- },
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  {
+    "FotiadisM/tabset.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("tabset").setup(
+        {
+          defaults = {
+            tabwidth = 2,
+            expandtab = true
+          },
+          languages = {
+            lua = {
+              tabwidth = 2,
+              expandtab = true
+            },
+            go = {
+              tabwidth = 4,
+              expandtab = false
+            },
+            {
+              filetypes = {
+                "javascript",
+                "typescript",
+                "javascriptreact",
+                "typescriptreact",
+                "json",
+                "yaml"
+              },
+              config = {
+                tabwidth = 2
+              }
+            }
+          }
+        }
+      )
+    end
+  },
 }
